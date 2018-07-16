0156c1083e13011a1e505cbfe4b39db7ad5bd337
Mike Smith <smithmd@interlochen.org>
2018-07-16 12:35:08 -0400

    adding services and some more data

--- src/app/_classes/AcademicTrack.ts
+++ src/app/_classes/AcademicTrack.ts
@@ -1,3 +1,20 @@
-export class AcademicTrack {
+import {AcademicTrackSelection} from './AcademicTrackSelection';
 
+export class AcademicTrack {
+  oid: string;
+  name: string;
+  maxCredit: number;
+  minCredit: number;
+  type: string;
+  selections: Array<AcademicTrackSelection> = [];
+
+  public static createFromJson(json: any): AcademicTrack {
+    const academicTrack = new AcademicTrack();
+    Object.assign(academicTrack, json);
+    return academicTrack;
+  }
+
+  addSelection(selection: AcademicTrackSelection): void {
+    this.selections.push(selection);
+  }
 }
--- src/app/_classes/AcademicTrackSelection.ts
+++ src/app/_classes/AcademicTrackSelection.ts
@@ -1,2 +1,11 @@
 export class AcademicTrackSelection {
+  oid: string;
+  selectionName: string;
+  subjectArea: string;
+  maxSelections: number;
+  minSelections: number;
+  allowAlternates: boolean;
+  sequenceNumber: number;
+  instructions: string;
+  gradeLevel: number;
 }
