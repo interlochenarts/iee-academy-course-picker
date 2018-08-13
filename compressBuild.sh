ce3f0cd07e791ef02816cd376a96d05413ea24d9
Mike Smith <smithmd@interlochen.org>
2018-08-13 14:50:59 -0400

    added build files

---
+++ compressBuild.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FILE='IEE_AcademyRequestsAngular'
+EXT='resource'
+
+# remove old zip file if it exists
+if [ -f Salesforce/src/staticresources/$FILE.$EXT ]; then
+   rm Salesforce/src/staticresources/$FILE.$EXT;
+   echo -e "removed old version of $FILE.$EXT\n"
+else
+   echo -e "$FILE.$EXT does not exist\n"
+fi
+
+cd dist
+
+# compress new version
+echo -e "Compressing files to $FILE.$EXT"
+zip -r "../Salesforce/src/staticresources/$FILE.$EXT" *
+
+echo -e "\nFinished creating $FILE.$EXT\n"
