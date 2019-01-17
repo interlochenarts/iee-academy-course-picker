6522250acf1baf425fea64d696af8c2a09beb3e7
Mike Smith <smithmd@interlochen.org>
2019-01-17 14:27:10 -0500

    review & submit page starting to look better

--- src/app/components/review-and-submit/review-and-submit.component.ts
+++ src/app/components/review-and-submit/review-and-submit.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {AcademicTrackSelection} from '../../classes/AcademicTrackSelection';
 import {AcademicTrackCourseSelection} from '../../classes/AcademicTrackCourseSelection';
 
+declare const Visualforce: any;
+
 @Component({
   selector: 'app-review-and-submit',
   templateUrl: './review-and-submit.component.html',
@@ -10,14 +12,19 @@ import {AcademicTrackCourseSelection} from '../../classes/AcademicTrackCourseSel
 export class ReviewAndSubmitComponent implements OnInit {
   @Input() educationId: string;
   @Input() trackSelectionsBySemester: Map<string, AcademicTrackSelection[]>;
-  selectedPrimaryCoursesBySemester: Map<string, AcademicTrackCourseSelection[]>;
-  selectedAlternateCoursesBySemester: Map<string, AcademicTrackCourseSelection[]>;
+  selectedPrimaryCoursesBySemester: Map<string, AcademicTrackCourseSelection[]> = new Map<string, AcademicTrackCourseSelection[]>();
+  selectedAlternateCoursesBySemester: Map<string, AcademicTrackCourseSelection[]> = new Map<string, AcademicTrackCourseSelection[]>();
   semesters: Array<string> = [];
+  readyToSubmit = false;
+  semesterComplete: Map<string, boolean> = new Map<string, boolean>();
+  semesterHasAlternates: Map<string, boolean> = new Map<string, boolean>();
+  submitting = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    this.semesters = Array.from(this.selectedPrimaryCoursesBySemester.keys());
+    this.semesters = Array.from(this.trackSelectionsBySemester.keys());
     // iterate over all values in the track selections map
     this.trackSelectionsBySemester.forEach((trackSelections: AcademicTrackSelection[], semester: string) => {
       trackSelections.forEach(ts => {
@@ -28,7 +35,45 @@ export class ReviewAndSubmitComponent implements OnInit {
         // check each course selection for alternate choices and add to alternate choices map for this semester
         const alternateChoices = ts.courseSelections.filter(c => c.isAlternateSelection);
         this.selectedAlternateCoursesBySemester.set(semester, alternateChoices);
+
+        this.semesterComplete.set(semester, trackSelections.reduce((complete, trackSelection) => {
+          return complete && (trackSelection.selectedCount >= trackSelection.minSelections);
+        }, true));
+
+        this.semesterHasAlternates.set(semester, trackSelections.reduce((hasAlternates, trackSelections) => {
+          return hasAlternates || trackSelections.allowAlternates;
+        }, false));
       });
     });
+  }
+
+  canClickCheckbox(): boolean {
+    return Array.from(this.semesterComplete.values()).reduce((complete, semCom) => {
+      return complete && semCom;
+    }, false);
+  }
+
+  onClickCheckbox() {
+    if (this.canClickCheckbox()) {
+      this.readyToSubmit = this.readyToSubmit !== true;
+    }
+  }
+
+  onSubmit() {
+    this.submitting = true;
+    Visualforce.remoting.Manager.invokeAction(
+      'IEE_ElectivePicker_Controller.saveElectiveComplete',
+      this.educationId,
+      (saved: boolean) => {
+        // redirect on true
+        if (saved) {
+          window.location.href = 'IEE_AcademyCoursesRequested?eid=' + this.educationId;
+        } else {
+          this.submitting = false;
+          console.log('something went wrong while submitting');
+        }
+      },
+      {buffer: false, escape: false}
+    );
   }
 }
