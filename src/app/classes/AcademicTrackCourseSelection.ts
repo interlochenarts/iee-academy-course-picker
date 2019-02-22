ce90269de4d850efc2845ebeff23397d12861729
Mike Smith <smithmd@interlochen.org>
2019-02-22 15:02:21 -0500

    adding semester to save method call

--- src/app/classes/AcademicTrackCourseSelection.ts
+++ src/app/classes/AcademicTrackCourseSelection.ts
@@ -26,7 +26,8 @@ export class AcademicTrackCourseSelection {
   }
 
   // should probably return a promise
-  public addOrRemoveRequest(educationId: string, updating: BehaviorSubject<boolean>, saveRelated: boolean): Promise<boolean> {
+  public addOrRemoveRequest(educationId: string, semester: string,
+                            updating: BehaviorSubject<boolean>, saveRelated: boolean): Promise<boolean> {
     const requestType: string = (this.isAlternateSelection ? 'Alternate' : (this.isPrimarySelection ? 'Primary' : 'none'));
     const deleteRequest: boolean = requestType === 'none';
     updating.next(true);
@@ -37,13 +38,14 @@ export class AcademicTrackCourseSelection {
         educationId,
         this.courseOfferingId,
         requestType,
+        semester,
         deleteRequest,
         (savedId: string) => {
           this.courseRequestId = savedId;
           if (saveRelated && this.relatedCourse) {
             this.relatedCourse.isPrimarySelection = this.isPrimarySelection;
             this.relatedCourse.isAlternateSelection = this.isAlternateSelection;
-            this.relatedCourse.addOrRemoveRequest(educationId, updating, false).then(r => resolve(true));
+            this.relatedCourse.addOrRemoveRequest(educationId, semester, updating, false).then(r => resolve(true));
           } else {
             updating.next(false);
             resolve(true);
