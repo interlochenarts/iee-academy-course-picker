f6b68bde33016e07a2951f68abc9da1eabfbdc3a
imbmoconno <brian.oconnor@interlochen.org>
2019-05-02 16:24:38 -0400

    Prevent courses marked as "default" from being unchecked

--- src/app/components/academic-track/course-checkbox/course-checkbox.component.ts
+++ src/app/components/academic-track/course-checkbox/course-checkbox.component.ts
@@ -47,9 +47,9 @@ export class CourseCheckboxComponent implements OnInit {
   isDisabled(): boolean {
     if (this.isPrimary) {
       return this.anyCourseUpdating === true || this.course.isAlternateSelection || !this.course.isPrimarySelection
-        && (this.selection.selectedCount >= this.selection.maxSelections);
+        && (this.selection.selectedCount >= this.selection.maxSelections) || this.course.defaultIndicator;
     } else {
-      return this.anyCourseUpdating === true || this.course.isPrimarySelection;
+      return this.anyCourseUpdating === true || this.course.isPrimarySelection || this.course.defaultIndicator;
     }
   }
 
