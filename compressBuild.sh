52e82bd9212e0fb2d8e8ffce1268c99f8ddccae6
Mike Smith <smithmd@interlochen.org>
2021-04-09 14:00:56 -0400

    tweak compressBuild script

--- compressBuild.sh
+++ compressBuild.sh
@@ -3,6 +3,7 @@
 FILE='IEE_AcademyRequestsAngular'
 EXT='resource'
 FOLDER='Salesforce/src/staticresources'
+DIST_FOLDER='dist/iee-academy-course-picker'
 
 # remove old zip file if it exists
 if [ -f $FOLDER/$FILE.$EXT ]; then
@@ -16,10 +17,14 @@ if [ ! -d "$FOLDER" ]; then
    mkdir -p $FOLDER;
 fi
 
-cd dist/iee-academy-course-picker
+cd $DIST_FOLDER || exit
 
 # compress new version
 echo -e "Compressing files to $FILE.$EXT"
 zip -r "../../$FOLDER/$FILE.$EXT" *
 
-echo -e "\nFinished creating $FILE.$EXT\n"
+if [ ! -f "../../$FOLDER/$FILE.$EXT" ]; then
+  >&2 echo -e "Failed to create resource"
+else
+  echo -e "\nFinished creating $FILE.$EXT\n"
+fi
