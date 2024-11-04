25dbd3c9c80fd0e8f9d97dcd938f4ae6ef0847e1
Mike Smith <smithmd@interlochen.org>
2024-11-04 15:38:15 -0500

    add quotes

--- gocd_scripts/deploy.sh
+++ gocd_scripts/deploy.sh
@@ -37,6 +37,6 @@ auth=("${SF}" auth jwt grant --client-id="${SFDC_CONSUMER_KEY}" --jwt-key-file="
 echo -e "${auth[@]}"
 "${auth[@]}" # run the auth command
 
-deploy=("${SF}" project deploy start --metadata-dir=../Salesforce/src --target-org="${DX_ENV}" --wait=60)
+deploy=("${SF}" project deploy start --metadata-dir="../Salesforce/src" --target-org="${DX_ENV}" --wait=60)
 echo -e "${deploy[@]}"
 "${deploy[@]}" # run the deploy command
